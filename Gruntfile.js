95c2ae42ccf83ce8841016e10515ded222f0d4de
mattmoehr <mattmoehr@gmail.com>
2015-10-16 14:17:40 -0500

    switched my livereload packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,84 +1,51 @@
 // Matt Moehr
 // Oct 16, 2015
-// stolen from http://rhumaric.com/2013/05/reloading-magic-with-grunt-and-livereload/
+// stolen from http://thecrumb.com/2014/03/15/using-grunt-for-live-reload/
 
 module.exports = function(grunt) {
+// Load Grunt tasks declared in the package.json file
+require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  // Load Grunt tasks declared in the package.json file
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+// Configure Grunt
+grunt.initConfig({
 
-  // Configure Grunt 
-  grunt.initConfig({
-
-    // grunt-contrib-connect will serve the files of the project
-    // on specified port and hostname
-     connect: {
-      options: {
-        port: 9000,
-        open: true,
-        livereload: 35729,
-        // Change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
-      },
-      livereload: {
+// Grunt express - our webserver
+// https://github.com/blai/grunt-express
+express: {
+    all: {
         options: {
-          middleware: function(connect) {
-            return [
-              connect.static('.tmp'),
-              connect().use('/bower_components', connect.static('./bower_components')),
-              connect.static('/')
-            ];
-          }
+            bases: ['C:\\Users\\moehr\\Documents\\GitHub\\data-dissemination-portal'],
+            port: 8080,
+            hostname: "0.0.0.0",
+            livereload: true
         }
-      }
-    },
-
-
-    // grunt-open will open your browser at the project's URL
-    open: {
-      all: {
-        // Gets the port from the connect configuration
-        path: 'http://localhost:<%= connect.all.options.port%>'
-      }
-    },
-
-    // Add vendor prefixed styles
-    autoprefixer: {
-      options: {
-        browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1']
-      },
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '.tmp/styles/',
-          src: '{,*/}*.css',
-          dest: '.tmp/styles/'
-        }]
-      }
-    },
-
-    watch: {
-      livereload: {
-        options: {
-          livereload: true
-        },
-        files: [
-          '/{,*/}*.html',
-          '/{,*/}*.css',
-          '/{,*/}*.js'
-        ] 
-      }
     }
-  });
+},
 
-  // Creates the `serve` task
-  grunt.registerTask('serve', 'start the server and preview your app', 
-                     function (target) {
-    grunt.task.run([
-      'concurrent:server',
-      'autoprefixer',
-      'connect:livereload',
-      'watch'
+// grunt-watch will monitor the projects files
+// https://github.com/gruntjs/grunt-contrib-watch
+watch: {
+    all: {
+            files: '**/*.html',
+            options: {
+                livereload: true
+        }
+    }
+},
+
+// grunt-open will open your browser at the project's URL
+// https://www.npmjs.org/package/grunt-open
+open: {
+    all: {
+        path: 'http://localhost:8080/index.html'
+    }
+}
+});
+
+// Creates the `server` task
+grunt.registerTask('server', [
+    'express',
+    'open',
+    'watch'
     ]);
-  });
 };

e8a6f8b5845b08527ce71c16d00231fae092f7d6
mattmoehr <mattmoehr@gmail.com>
2015-11-03 12:07:43 -0600

    modular-izing the html

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<nav role="navigation" class="navbar navbar-sm navbar-default navbar-fixed-top">
+
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <span class="navbar-brand">SHOW Data Dissemination</span>
+  </div>
+
+  <!-- Collection of nav links and other content for toggling -->
+  <div id="navbarCollapse" class="collapse navbar-collapse">
+    <ul class="nav navbar-nav navbar-right">
+      <li class=""><a href="#">Home</a></li>
+      <li class=""><a href="./charts">Charts</a></li>
+      <li class=""><a href="./codebooks">Codebooks</a></li>
+      <li class=""><a href="./documents">Documentation</a></li>
+      <li class=""><a href="./contact">Contact</a></li>
+    </ul>
+  </div>
+</nav>

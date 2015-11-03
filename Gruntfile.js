9b9bd3ccc2ade1060f211c6e075251d492cd23cd
mattmoehr <mattmoehr@gmail.com>
2015-11-03 17:56:35 -0600

    switched up the livereload setup
    started using grunt-browser-sync and grunt-php even though browserSync
    required me to install visual studio 2013. (gross)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,66 +1,42 @@
 // Matt Moehr
-// Oct 16, 2015
-// stolen from http://thecrumb.com/2014/03/15/using-grunt-for-live-reload/
+// November 3, 2015
+// stole from: http://fettblog.eu/php-browsersync-grunt-gulp/
+
+'use strict';
 
 module.exports = function(grunt) {
-// Load Grunt tasks declared in the package.json file
-require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-php');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-// Configure Grunt
-grunt.initConfig({
-
-// Grunt express - our webserver
-// https://github.com/blai/grunt-express
-express: {
-    all: {
-        options: {
-            bases: ['C:\\Users\\moehr\\Documents\\GitHub\\data-dissemination-portal'],
-            port: 8080,
-            hostname: "0.0.0.0",
-            livereload: true
+    grunt.initConfig({
+        watch: {
+            php: {
+                files: ['**/*.php']
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: '**/*.php'
+                },
+                options: {
+                    proxy: '127.0.0.1:8010', //our PHP server
+                    port: 8080, // our new port
+                    open: true,
+                    watchTask: true
+                }
+            }
+        },
+        php: {
+            dev: {
+                options: {
+                    port: 8010,
+                    base: 'C:\\Users\\moehr\\Documents\\GitHub\\data-dissemination-portal'
+                }
+            }
         }
-    }
-},
+    });
 
-// PHP server
-// stolen from here http://fettblog.eu/php-browsersync-grunt-gulp/
-// php: {
-//     all: {
-//         options: {
-//             base: 'C:\\Users\\moehr\\Documents\\GitHub\\data-dissemination-portal',
-//             hostname: "127.0.0.1",
-//             port: 8080,
-//             keepalive: false,
-//             open: true
-//         }
-//     }
-// },
-
-
-// grunt-watch will monitor the projects files
-// https://github.com/gruntjs/grunt-contrib-watch
-watch: {
-    all: {
-            files: '**/*.html',
-            options: {
-                livereload: true
-        }
-    }
-},
-
-// grunt-open will open your browser at the project's URL
-// https://www.npmjs.org/package/grunt-open
-open: {
-    all: {
-        path: 'http://localhost:8080/index.html'
-    }
-}
-});
-
-// Creates the `server` task
-grunt.registerTask('server', [
-    'express',
-    'open',
-    'watch'
-    ]);
+    grunt.registerTask('default', ['php', 'browserSync', 'watch']);
 };

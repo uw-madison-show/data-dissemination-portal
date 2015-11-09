1f73971fd860810e5ac75c4236cd18885b41cd69
mattmoehr <mattmoehr@gmail.com>
2015-11-09 11:44:40 -0600

    placeholder for the contact page

---
+++ data/contact/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php
+      $include_folder = $_SERVER['DOCUMENT_ROOT'] . '/data/includes/';
+      include( $include_folder . 'head.php' );
+    ?>
+    
+  </head>
+  <!--page content-->
+  <body id="page-top" style="padding-top: 55px;">
+    <a href="#content" class="sr-only">skip to content</a>
+
+    <header>
+      <?php include( ($include_folder . 'navbar.php') ); ?>
+    </header>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-6 col-xs-offset-3">
+          <p>
+            Get in touch with us if you like.
+          </p>
+        </div>
+
+</body>
+</html>
